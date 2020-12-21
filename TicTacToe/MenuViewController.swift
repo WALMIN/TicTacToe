e1250fd3834b24b1a1b5436e6db1052e9e5dd192
Victor Bergene <v@walmin.com>
2020-12-21 11:16:54 +0100

    Play with friend menu button starts a game now

--- TicTacToe/MenuViewController.swift
+++ TicTacToe/MenuViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
+    let seguePlayWithFriend = "PlayWithFriend"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
 
+    @IBAction func playButton(_ sender: UIButton) {
+        performSegue(withIdentifier: seguePlayWithFriend, sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == seguePlayWithFriend {
+            _ = segue.destination as! ViewController
+                        
+        }
+    
+    }
+    
 }
