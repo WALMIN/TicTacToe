91b2b067d394af8a2abd01c0675a3769b3f02afa
Victor Bergene <v@walmin.com>
2021-01-04 12:17:54 +0100

    Added views to the about page

---
+++ TicTacToe/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  TicTacToe
+//
+//  Created by Victor Bergene on 2021-01-04.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
